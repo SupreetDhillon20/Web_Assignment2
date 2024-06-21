3df3d9d7907e3af0aed74aa5a9cc2eeabc91c82d
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-06-21 02:28:47 -0600

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+import Form from "./components/form";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center mx-10">
+      <Form/>
+    </main>
+  );
+}
