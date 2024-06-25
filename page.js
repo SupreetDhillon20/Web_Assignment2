9f9e1943c6a0ddef088cee98391ee334accb76a1
Sahilpreetsingh004 <Sahilpreetsingh@edu.sait.ca>
2024-06-25 17:40:56 -0600

    Update page.js

--- page.js
+++ page.js
@@ -1,7 +1,10 @@
 import Form from "./components/form";
+import StudentList from '../components/StudentList';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center mx-10">
+      <StudentList />
       <Form/>
     </main>
   );
