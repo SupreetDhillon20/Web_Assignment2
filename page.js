19c9f903d66964818f509489d4a624486ae4f34f
Bir <140426404+Tarunbir003@users.noreply.github.com>
2024-06-26 02:47:05 -0600

    page.js

--- page.js
+++ page.js
@@ -1,7 +1,24 @@
 'use client'
 import Form from "./components/form";
 import StudentList from '../components/StudentList';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+import Footer from './components/Footer';
 
+const Page = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="container mx-auto">
+                <Form />
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default Page;
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center mx-10">
