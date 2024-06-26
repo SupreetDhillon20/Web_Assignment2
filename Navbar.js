195bda4fa01724930de970c357e8b59678d2fb09
Bir <140426404+Tarunbir003@users.noreply.github.com>
2024-06-26 02:48:09 -0600

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="bg-blue-500 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-white text-2xl font-bold">
+                    My Application
+                </div>
+                <div className="space-x-4">
+                    <a href="/" className="text-white hover:text-gray-200">Home</a>
+                    <a href="/about" className="text-white hover:text-gray-200">About</a>
+                    <a href="/contact" className="text-white hover:text-gray-200">Contact</a>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
