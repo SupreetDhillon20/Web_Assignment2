ca42e21f6a49beb8d35ca31facc345412f2fe037
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-06-26 23:53:14 -0600

    Add files via upload

--- page.js
+++ page.js
@@ -1,21 +1,12 @@
-import React from "react";
-import StudentInfo from "./components/StudentInfo";
+import React from 'react';
+import Form from '../components/Form';
 
-export default function Home() {
+const FormPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center mx-10">
-      <div className="bg-gray-100 rounded-md">
-      <div className="container mx-auto py-8">
-        <div className="text-center mb-8">
-          <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to Williamson High School</h1>
-          <p className="text-lg text-gray-600">
-            At Williamson High School, we are committed to nurturing the next generation of leaders, thinkers, and innovators.
-          </p>
-        </div>
-
-      </div>
+    <div className="flex min-h-screen flex-col items-center mx-10">
+      <Form />
     </div>
-      <StudentInfo/>
-    </main>
-  );
+  )
 }
+
+export default FormPage;
