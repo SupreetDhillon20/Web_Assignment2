c2e283a33ba7d1e0ca1b81b5a80cc95ec2fe4209
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-06-26 22:48:13 -0600

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,15 +2,40 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 p-4 mt-10">
+        <footer className="bg-gray-700 p-4 mt-10">
             <div className="container mx-auto text-center text-white">
-                <p>&copy; 2024 My Application. All rights reserved.</p>
-                <p>
-                    Follow us on 
-                    <a href="https://twitter.com" className="text-blue-400 hover:text-blue-600 ml-2">Twitter</a>, 
-                    <a href="https://facebook.com" className="text-blue-400 hover:text-blue-600 ml-2">Facebook</a>, 
-                    <a href="https://instagram.com" className="text-blue-400 hover:text-blue-600 ml-2">Instagram</a>
-                </p>
+                <div className="mb-4">
+                    <p>&copy; 2024 Williamson High School. All rights reserved.</p>
+                </div>
+                <div className="mb-4 text-left">
+                    <p>
+                        Address:<br />
+                        123 Main Street,<br />
+                        Calgary, AB, T1K 1L3
+                    </p>
+                </div>
+                <div className="mb-4 text-left">
+                    <p>
+                        Contact Information:<br />
+                        Phone: (338) 456-7890<br />
+                        Email: <a href="mailto:info@abchighschool.edu" className='text-white hover:text-blue-500'>info@williamsonhighschool.edu</a>
+                    </p>
+                </div>
+                <div className="mb-4 text-left">
+                    <p>
+                        Office Hours:<br />
+                        Monday - Friday: 8:00 AM - 4:00 PM<br />
+                        Saturday - Sunday: Closed
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        Follow Us:<br />
+                        <a href="https://facebook.com/abchighschool" className="text-white hover:text-blue-500 ml-2">Facebook</a>, 
+                        <a href="https://twitter.com/abchighschool" className="text-white hover:text-blue-500 ml-2">Twitter</a>, 
+                        <a href="https://instagram.com/abchighschool" className="text-white hover:text-blue-500 ml-2">Instagram</a>
+                    </p>
+                </div>
             </div>
         </footer>
     );
