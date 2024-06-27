9869309fad1a2c124af043657be81041fd4435e5
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-06-27 00:20:29 -0600

    Update page.js

--- page.js
+++ page.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import StudentList from '../components/StudentList'
+import React from "react";
+import StudentInfo from "./components/StudentInfo";
 
-const Students = () => {
+export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center mx-10">
-      <StudentList />
-    </div>
-  )
-}
+    <main className="flex min-h-screen flex-col items-center mx-10">
+      <div className="bg-gray-100 rounded-md">
+      <div className="container mx-auto py-8">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to Williamson High School</h1>
+          <p className="text-lg text-gray-600">
+            At Williamson High School, we are committed to nurturing the next generation of leaders, thinkers, and innovators.
+          </p>
+        </div>
 
-export default Students;
+      </div>
+    </div>
+      <StudentInfo/>
+    </main>
+  );
+}
