17216ebaa32149f1075ad463cbc33d096fdfdba6
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-06-26 22:46:32 -0600

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -3,24 +3,29 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-500 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-2xl font-bold">
-                    My Application
-                </div>
-                <div className="space-x-4">
-                    <Link href="/">
-                        <a className="text-white hover:text-gray-200">Home</a>
-                    </Link>
-                    <Link href="/form">
-                        <a className="text-white hover:text-gray-200">Form</a>
-                    </Link>
-                    <Link href="/students">
-                        <a className="text-white hover:text-gray-200">Students</a>
-                    </Link>
-                </div>
+        <nav className="bg-gray-800 h-20 py-5 mb-4 items-center">
+          <div className="container mx-auto flex justify-between items-center"> 
+            {/* School Name */}
+            <div className="text-white text-2xl font-semibold">
+              Williamson High School
             </div>
-        </nav>
+
+            <div className="space-x-9">
+              {/* Home Link */}
+              <Link href="/" passHref>
+                <span className="text-white font-medium hover:underline cursor-pointer">Home</span>
+              </Link>
+              {/* Form Link */}
+              <Link href="/form" passHref>
+                <span className="text-white font-medium hover:underline cursor-pointer">Form</span>
+              </Link>
+              {/* Students Link */}
+              <Link href="/students" passHref>
+                <span className="text-white font-medium hover:underline cursor-pointer">Students</span>
+              </Link>
+            </div>
+          </div>
+    </nav>
     );
 };
 
